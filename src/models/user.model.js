f7d78217d9bdfb6dcf6f342015ba9840b22b3bdf
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-02-22 23:16:51 +0530

    password encryption and token generation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -43,5 +45,52 @@ const userSchema = new mongoose.Schema({
         type: String
     }
 }, { timestamps: true });
+
+// Encrypting password just before saving it in DB (using pre hook, a middleware of mongoose) 
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+
+    next();
+})
+
+// Defining a method to check the password
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+// Defining a method for generating Access token
+userSchema.methods.generateAccessToken = function () {
+
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+}
+
+// Defining a method for generating Refresh token
+userSchema.methods.generateRefreshToken = function () {
+
+    return jwt.sign(
+        {
+            _id: this.id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+}
 
 export const User = mongoose.model("User", userSchema);
