e14b57b61519fe96a578526bc5ba096a9f0d502b
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-02-23 20:02:12 +0530

    routes added in app.js

--- src/app.js
+++ src/app.js
@@ -15,4 +15,22 @@ app.use(express.static("public"));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+import commentRouter from "./routes/comment.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import likeRouter from "./routes/likes.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import userRouter from './routes/user.routes.js'
+import videoRouter from "./routes/video.routes.js"
+
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/video", videoRouter);
+
 export default app
